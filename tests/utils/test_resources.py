e5dbf02f12e2d40fa4249e3c3f2cb94474772b3a
Patrik Smeds <patrik.smeds@gmail.com>
2021-09-22 15:13:50 +0200

    Feat: new helper functions used to load resources and update config

---
+++ tests/utils/test_resources.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+
+import unittest
+import yaml
+
+
+class TestMiscUtils(unittest.TestCase):
+    def setUp(self):
+        with open("tests/utils/files/configs.yaml") as file:
+            self.config = yaml.load(file)
+
+    def tearDown(self):
+        pass
+
+    def test_merge(self):
+        from hydra_genetics.utils.resources import load_resources
+        self.assertEqual({'bwa_mem': {'extra': "some settings"}}, self.config)
+        self.config = load_resources(self.config, "tests/utils/files/resources.yaml")
+        self.assertEqual({'bwa_mem': {'extra': "some settings", 'cpu': 20, 'time': '12:00:00'}}, self.config)

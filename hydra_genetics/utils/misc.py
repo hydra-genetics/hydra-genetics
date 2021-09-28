87ba7058c4f74c8988a73ae5d869a5c194a29378
Patrik (Eriksson) Smeds <patrik.smeds@gmail.com>
2021-09-28 08:41:29 +0200

    Update misc.py

--- hydra_genetics/utils/misc.py
+++ hydra_genetics/utils/misc.py
@@ -8,15 +8,9 @@ def merge(dict1, dict2):
     ''' Return a new dictionary by merging two dictionaries recursively. '''
 
     result = deepcopy(dict1)
-    print("Reuslt...")
-    print(result)
     for key, value in dict2.items():
-        print(key)
         if isinstance(value, collections.Mapping):
             result[key] = merge(result.get(key, {}), value)
         else:
-            print("Add")
             result[key] = deepcopy(dict2[key])
-    print("Reuslt...")
-    print(result)
     return result

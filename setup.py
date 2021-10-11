4dd14c37ada6f4f1f13032fdb57326b5b5394c90
Patrik (Eriksson) Smeds <patrik.smeds@gmail.com>
2021-10-11 15:15:01 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,9 +10,6 @@ with open("README.md") as f:
 with open("LICENSE.md") as f:
     license = f.read()    
     
-with open("requirements.txt") as f:
-    required = f.read().splitlines()
-
 setup(
     name="hydra-genetics",
     version=version,
@@ -34,9 +31,15 @@ setup(
     author="Patrik Smeds",
     author_email="patrik.smeds@scilifelab.uu.se",
     url="https://github.com/hydra-genetics/tools",
-    license=license,
+    license='GPL-3,
     entry_points={"console_scripts": ["hydra-genetics=hydra_genetics.__main__:run"]},
-    install_requires=required,
+    install_requires=[
+        'pandas==1.3.1',
+        'click==7.1.2',
+        'jinja2==3.0.1',
+        'rich==10.9.0',
+        'gitpython'
+    ],
     setup_requires=["twine>=1.11.0", "setuptools>=38.6."],
     packages=find_packages(exclude=("docs")),
     include_package_data=True,

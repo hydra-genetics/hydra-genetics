d6274d2797c74f487cbae1b8d455640c761ae259
Patrik (Eriksson) Smeds <patrik.smeds@gmail.com>
2021-10-21 07:49:23 +0200

    Update samples.py

--- hydra_genetics/utils/samples.py
+++ hydra_genetics/utils/samples.py
@@ -4,6 +4,22 @@ import typing
 import pandas
 
 
+def get_sample(samples: pandas.DataFrame, wildcards: snakemake.io.Wildcards) -> pandas.Series:
+    """
+    function used to extract one sample(row) from sample.tsv
+    Args:
+        samples: DataFrame generate by importing a file following schema defintion
+               found in pre-alignment/workflow/schemas/samples.schema.tsv
+        wildcards: wildcards object with at least the following wildcard names
+               sample
+    Returns:
+        Series containing data of the selected row
+    Raises:
+        raises an exception (KeyError) if no sample can be extracted from the Dataframe
+    """
+    return samples.loc[(wildcards.sample)].dropna()
+
+
 def get_samples(samples: pandas.DataFrame) -> typing.List[str]:
     """
     function used to extract all sample found in samples.tsv

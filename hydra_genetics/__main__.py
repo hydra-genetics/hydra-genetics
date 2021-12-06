b441905d487dba28058ea155680bbf6e7b8edfe8
Patrik Smeds <patrik.smeds@gmail.com>
2021-12-06 15:05:37 +0100

    create inpute file tool.

--- hydra_genetics/__main__.py
+++ hydra_genetics/__main__.py
@@ -6,7 +6,7 @@ import re
 import sys
 
 import hydra_genetics.utils
-from hydra_genetics.commands.create import PipelineCreate, RuleCreate
+from hydra_genetics.commands.create import PipelineCreate, RuleCreate, CreateInputFiles
 import rich.console
 import rich.logging
 import rich.traceback
@@ -127,14 +127,108 @@ def create_module(name, description, author, email, version, min_snakemake_versi
     callback=validate_wf_name_prompt,
     type=str,
     help="name module/workflow where rule will be added. Expected folder structure is module_name/workflow/, "
-         " the rule will be added to a subfolder named rules, env.yaml to a subfolder named envs.",
-)
-@click.option("-a", "--author", prompt=True, required=True, type=str, help="Name of the main author(s)")
-@click.option("-e", "--email", prompt=True, required=True, type=str, help="E-mail(s) of the main author(s)")
-@click.option("-o", "--outdir", type=str, help="Output directory for where rule will be added (default: current dir)")
+         " the rule will be added to a subfolder named rules, env.yaml to a subfolder named envs.")
+@click.option(
+        "-a",
+        "--author",
+        prompt=True,
+        required=True,
+        type=str,
+        help="Name of the main author(s)")
+@click.option(
+        "-e",
+        "--email",
+        prompt=True,
+        required=True,
+        type=str,
+        help="E-mail(s) of the main author(s)")
+@click.option(
+        "-o",
+        "--outdir",
+        type=str,
+        help="Output directory for where rule will be added (default: current dir)")
 def create_rule(name, module, author, email, outdir):
     rule = RuleCreate(name, module, author, email, outdir)
     rule.init_rule()
+
+
+@cli.command(short_help="create inpu-files, samples.tsv and units.tsv")
+@click.option(
+    "-d",
+    "--directory",
+    multiple=True,
+    prompt="directory/directories",
+    required=True,
+    type=str,
+    help="path to dir where fastq-files should be looked for.")
+@click.option(
+        "-o",
+        "--outdir",
+        type=str,
+        help="Output directory for where rule will be added (default: current dir)")
+@click.option(
+        "-p",
+        "--platform",
+        type=str,
+        help="Sequence platform that the data originate from, ex nextseq, miseq. Default Illumina",
+        default="Illumina")
+@click.option(
+        "-r",
+        "--run",
+        type=str,
+        help="Sequence run id, something used to identify where data originate from, ex data, flowcell id. Could also be a regex "
+             "used to extract run id from file path (need to contain '(' and ')', ex \".*([0-0]+_[A_Z]+).*\".",
+        default="RUN")
+@click.option(
+        "-l",
+        "--lane-identifier",
+        type=str,
+        help="Identifier for lane, either a string or a regex that will extract lane identifier from file "
+             "name, ex \"_(L[0-9]*)_\"",
+        default="L000")
+@click.option(
+        "-t",
+        "--sample-type",
+        type=str,
+        help="Sample type, N|T|R, default T",
+        default="T")
+@click.option(
+        "-s",
+        "--sample-regex",
+        type=str,
+        help="Regex used to extract sample from filename, default '([A-Za-z0-9-]+)_S[0-9]+_R[12]{1}_001.fastq.gz$'",
+        default="^([A-Za-z0-9-]+)_S[0-9]+_(R[12]{1})_001.fastq.gz")
+@click.option(
+        "-n",
+        "--read-number-regex",
+        type=str,
+        help="Regex used to extract read number from filename (note only number value), default '_R([1-2]{1})_001'",
+        default="_R([12]{1})_")
+@click.option(
+        "-a",
+        "--adapters",
+        type=str,
+        help="adapter sequence, comma sepearated",
+        default="AGATCGGAAGAGCACACGTCTGAACTCCAGTCA,AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT")
+@click.option(
+        "--post-file-modifier",
+        type=str,
+        help="add string to output files",
+        default=None)
+@click.option(
+        "--force",
+        help="overwrite existing files",
+        is_flag=True)
+@click.option(
+        "--tc",
+        help="tumor contet",
+        type=float,
+        default=1.0)
+def create_input_files(directory, outdir, post_file_modifier, platform, run, sample_type,
+                       sample_regex, read_number_regex, lane_identifier, adapters, tc, force):
+    input_files = CreateInputFiles(directory, outdir, post_file_modifier, platform, run, sample_type,
+                                   sample_regex, read_number_regex, lane_identifier, adapters, tc, force)
+    input_files.init_rule()
 
 
 @cli.command(short_help="download reference data")
--- hydra_genetics/commands/create.py
+++ hydra_genetics/commands/create.py
@@ -5,10 +5,12 @@ organization's specification based on a template.
 
 from datetime import date
 import git
+import glob
 import logging
 import jinja2
 import pathlib
 import os
+import re
 import sys
 import hydra_genetics
 
@@ -204,3 +206,137 @@ class RuleCreate(object):
             fh.seek(0)
             lines.insert(line_number, str(f"include: \"rules/{self.name}.smk\"\n"))
             fh.writelines(lines)
+
+
+class CreateInputFiles(object):
+    """Creates a hydra-genetics input files samples.tsv and units.tsv.
+    Args:
+        directories (str): Path to dir that should be search.
+        outdir (str): Path to the local output directory.
+    """
+    def __init__(self,
+                 directory,
+                 outdir=None,
+                 post_file_modifier=None,
+                 platform="Illumina",
+                 run="RUN",
+                 sample_type="T",
+                 sample_regex="^([A-Za-z0-9-]+)_S[0-9]+_R[12]{1}_001.fastq.gz",
+                 read_number_regex="^[A-Za-z0-9-]+_S[0-9]+_(R[12]{1})_001.fastq.gz",
+                 lane_identifier="_(L[0-9]+)_",
+                 adapters="AGATCGGAAGAGCACACGTCTGAACTCCAGTCA,AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT",
+                 tc=1.0,
+                 force=False):
+        self.directory = directory
+        self.outdir = outdir
+        self.post_file_modifier = post_file_modifier
+        self.run = run
+        self.platform = platform
+        self.sample_type = sample_type
+        self.sample_regex = sample_regex
+        self.read_number_regex = read_number_regex
+        self.lane_identifier = lane_identifier
+        self.adapters = adapters
+        self.tc = tc
+        self.force = force
+
+        if not self.outdir:
+            self.outdir = os.getcwd()
+
+    def init_rule(self):
+        """Creates the hydra_genetics rule."""
+        log.info(f"Searching for fastq-files:")
+        file_dict = {}
+        platform = self.platform
+        sample_type = self.sample_type
+        run = self.run
+        run_is_regex = True if re.search(".*[(].+[)].*", run) else False
+        for d in self.directory:
+            log.info(f"Dir: %s" % d)
+            for f in glob.glob('%s/**/*.fastq.gz' % d, recursive=True):
+                if "Undetermined" in f:
+                    continue
+                temp_filename = os.path.basename(f)
+                try:
+                    run_value = run
+                    if run_is_regex:
+                        run_value = re.search(run, f)
+                        if run_value:
+                            run_value = run_value.group(1)
+                        else:
+                            raise ValueError("Unable to extract run info from:\n{}\nwith regex: {}".format(temp_filename, run))
+                    samplename = re.search(self.sample_regex, temp_filename).group(1)
+                    read = re.search(self.read_number_regex, temp_filename).group(1)
+                    if run_value not in file_dict:
+                        file_dict[run_value] = {}
+                    if samplename in file_dict[run_value]:
+                        if read in file_dict[run_value][samplename]:
+                            raise Exception(
+                                    "Trying to add read (%s) for sample %s multiple times\n%s\n%s" %
+                                    (read, samplename, f, file_dict[samplename][read]))
+                        file_dict[run_value][samplename].append(f)
+                    else:
+                        file_dict[run_value][samplename] = [f]
+                    log.info("    found: %s" % f)
+                except AttributeError:
+                    log.debug("Couldn't extract sample name from: %s" % temp_filename)
+        lane = def f(x): return self.lane_identifier
+        if re.search(".*[(].+[)].*", self.lane_identifier):
+            lane = def f(x): return re.search(self.lane_identifier, x).group(1)
+        for run_key in file_dict:
+            for sample in file_dict[run_key]:
+                if len(file_dict[run_key][sample]) % 2 != 0:
+                    raise ValueError("Uneven number of files found:\n{}".format(str(file_dict[run_key][sample])))
+                lane_dict = dict()
+                no_index_counter = 0
+                for f in file_dict[run_key][sample]:
+                    read_number = re.search(self.read_number_regex, f).group(1)
+                    lane_id = lane(f)
+                    if lane_id in lane_dict:
+                        if read_number in lane_dict[lane_id]:
+                            raise ValueError(
+                                "s index and read number combination found multiple times: index {} read {}:\n - {}".format(
+                                     lane_id,
+                                     read_number,
+                                     "\n - ".join(
+                                         [f, lane_dict[lane_id][read_number]])))
+                        lane_dict[lane_id][read_number] = f
+                    else:
+                        lane_dict[lane_id] = {read_number: f}
+                file_dict[run_key][sample] = lane_dict
+        samples_file_name = "samples.tsv"
+        if self.post_file_modifier is not None:
+            samples_file_name = "samples_{}.tsv".format(self.post_file_modifier)
+        if os.path.isfile(samples_file_name):
+            if not self.force:
+                log.warn("File exists {} and force wasn't used".format(samples_file_name))
+                exit(1)
+            else:
+                log.warn("File exists {} overwriting!!!".format(samples_file_name))
+        with open(samples_file_name, "w") as output:
+            output.write("\t".join(["sample", "TC"]))
+            for run_key, data in sorted(file_dict.items()):
+                for sample, sample_info in sorted(data.items()):
+                    output.write("\n{}".format("\t".join([sample, str(self.tc)])))
+        units_file_name = "units.tsv"
+        if self.post_file_modifier is not None:
+            units_file_name = "units_{}.tsv".format(self.post_file_modifier)
+        if os.path.isfile(units_file_name):
+            if not self.force:
+                log.warn("File exists {} and force wasn't used".format(units_file_name))
+                exit(1)
+            else:
+                log.warn("File exists {} overwriting!!!".format(units_file_name))
+        with open(units_file_name, "w") as output:
+            output.write("\t".join(["sample", "type", "platform", "run", "lane", "fastq1", "fastq2", "adapter"]))
+            for run_key, values in sorted(file_dict.items()):
+                for sample, data in sorted(file_dict[run_key].items()):
+                    for lane in data:
+                        if len(data[lane].keys()) != 2:
+                            raise ValueError("Incorrect number of fastq-files: {}:\n - {}".format(
+                                len(data[sample][lane].keys()), "\n - ".join(
+                                    "{}: {}".format(k, data[lane][k]) for k in data[lane])))
+                        output.write("\n"+"\t".join([sample, self.sample_type, self.platform, run_key, lane,
+                                     str(data[lane]["1"]),
+                                     str(data[lane]["2"]),
+                                     self.adapters]))

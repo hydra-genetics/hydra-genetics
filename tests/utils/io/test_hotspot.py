3553c6e86c435592742a25ba52bc8feed9ed9e02
Patrik Smeds <patrik.smeds@gmail.com>
2021-12-17 09:05:21 +0100

    pycodestyle for testing.

--- tests/utils/io/test_hotspot.py
+++ tests/utils/io/test_hotspot.py
@@ -9,43 +9,42 @@ import unittest
 
 logger = logging.getLogger(__name__).addHandler(logging.NullHandler())
 
+
 class TestHotspots(unittest.TestCase):
     def setUp(self):
         # create fixtures
         self.tempdir = tempfile.mkdtemp()
-        with open(os.path.join(self.tempdir,"hotspot"),'w') as hotspots:
-            hotspots.write("#Chr\tStart\tEnd\tGene\tCDS_mutation_syntax\tAA_mutation_syntax\tReport\tcomment\tExon\tAccession_number\n")
+        with open(os.path.join(self.tempdir, "hotspot"), 'w') as hotspots:
+            hotspots.write("#Chr\tStart\tEnd\tGene\tCDS_mutation_syntax\tAA_mutation_syntax\tReport\tcomment\tExon\tAccession_number\n")  # noqa
             hotspots.write("NC_000001.10\t115252190\t115252349\tNRAS\t-\t-\tindel\t-\texon4\tNM_002524\n")
             hotspots.write("NC_000001.10\t115252202\t115252202\tNRAS\tc.C438\tp.A146\thotspot\t-\texon4\tNM_002524\n")
             hotspots.write("NC_000017.10\t37880979\t37881164\tERBB2\t-\t-\tindel\t-\texon20\tNM_004448\n")
             hotspots.write("NC_000012.11\t25398279\t25398279\tKRAS\tc.G40\tp.V14\tregion\t-\texon2\tNM_004985\n")
             hotspots.write("NC_000017.10\t37880986\t37880987\tERBB2\t-\tp.Y772\tregion_all\t-\texon20\tNM_004448\n")
 
-
     def tearDown(self):
         # delete fixtures
         shutil.rmtree(self.tempdir)
 
-
     def test_header_parsing(self):
-         from hydra_genetics.utils.io.hotspot import Reader
-         reader = Reader(os.path.join(self.tempdir,"hotspot"))
-         self.assertEqual(10, len(reader.header.keys()))
-         self.assertEqual(0, reader.header["CHR"])
-         self.assertEqual(1, reader.header["START"])
-         self.assertEqual(2, reader.header["END"])
-         self.assertEqual(3, reader.header["GENE"])
-         self.assertEqual(4, reader.header["CDS_MUTATION_SYNTAX"])
-         self.assertEqual(5, reader.header["AA_MUTATION_SYNTAX"])
-         self.assertEqual(6, reader.header["REPORT"])
-         self.assertEqual(7, reader.header["COMMENT"])
-         self.assertEqual(8, reader.header["EXON"])
-         self.assertEqual(9, reader.header["ACCESSION_NUMBER"])
+        from hydra_genetics.utils.io.hotspot import Reader
+        reader = Reader(os.path.join(self.tempdir, "hotspot"))
+        self.assertEqual(10, len(reader.header.keys()))
+        self.assertEqual(0, reader.header["CHR"])
+        self.assertEqual(1, reader.header["START"])
+        self.assertEqual(2, reader.header["END"])
+        self.assertEqual(3, reader.header["GENE"])
+        self.assertEqual(4, reader.header["CDS_MUTATION_SYNTAX"])
+        self.assertEqual(5, reader.header["AA_MUTATION_SYNTAX"])
+        self.assertEqual(6, reader.header["REPORT"])
+        self.assertEqual(7, reader.header["COMMENT"])
+        self.assertEqual(8, reader.header["EXON"])
+        self.assertEqual(9, reader.header["ACCESSION_NUMBER"])
 
     def test_record_parsing(self):
         from hydra_genetics.utils.io.hotspot import Reader
         from hydra_genetics.utils.models.hotspot import ReportClass
-        reader = Reader(os.path.join(self.tempdir,"hotspot"))
+        reader = Reader(os.path.join(self.tempdir, "hotspot"))
 
         record = reader.next()
         self.assertEqual("NC_000001.10", record.CHROMOSOME)
@@ -115,7 +114,7 @@ class TestHotspots(unittest.TestCase):
         self.assertEqual("exon20", record.EXON)
         self.assertEqual("NM_004448", record.ACCESSION_NUMBER)
         self.assertEqual(2, len(record.VARIANTS))
-        self.assertEqual([{'extended': False, "variants": []},{'extended': False, "variants": []}], record.VARIANTS)
+        self.assertEqual([{'extended': False, "variants": []}, {'extended': False, "variants": []}], record.VARIANTS)
 
         with self.assertRaises(StopIteration):
             reader.next()

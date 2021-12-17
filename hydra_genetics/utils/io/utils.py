628ced719a648d520cc5f9481dc22fb7de647afe
Patrik Smeds <patrik.smeds@gmail.com>
2021-12-17 08:31:14 +0100

    Fix codestyle.

--- hydra_genetics/utils/io/utils.py
+++ hydra_genetics/utils/io/utils.py
@@ -1,8 +1,10 @@
 import pysam
 from pysam import VariantFile
+import statistics
 import warnings
 
 from hydra_genetics.utils.models.hotspot import ReportClass
+
 
 def add_contigs_to_header(input_name, output_name, contig_file, assembly):
     from src.lib.data.files.reference import InfoImporter
@@ -13,6 +15,11 @@ def add_contigs_to_header(input_name, output_name, contig_file, assembly):
     output_vcf = VariantFile(output_name, 'w', header=input_vcf.header)
     for record in input_vcf:
         output_vcf.write(record)
+
+
+def get_annotation_data(data_extracter, mapper):
+    return lambda variant, field: data_extracter(variant, mapper[field])
+
 
 def get_annoation_data_vep(field_dict):
     def extractor(variant, info_name):
@@ -29,9 +36,16 @@ def get_annoation_data_vep(field_dict):
         return "-"
     return extractor
 
-def get_depth(variant, sample):
-    if isinstance(variant, pysam.VariantRecord):
-        return (get_sample_value(variant, sample)['DP'],) + get_sample_value(variant, sample)['AD']
+
+def get_depth(gvcf_file, sample, chr, start, stop):
+    depth = [r.samples[sample]['DP'] for r in gvcf_file.fetch(chr, start, stop)]
+    if len(depth) > 1:
+        return statistics.mean(depth)
+    elif len(depth) == 1:
+        return depth[0]
+    else:
+        return 0
+
 
 def get_info_field(variant, info_name):
     if isinstance(variant, pysam.VariantRecord):
@@ -46,6 +60,7 @@ def get_info_field(variant, info_name):
             return "-"
     return '-'
 
+
 def get_report_type(variant, hotspot):
     if isinstance(variant, pysam.VariantRecord):
         if hotspot.REPORT == ReportClass.region or hotspot.REPORT == ReportClass.region_all:
@@ -54,9 +69,11 @@ def get_report_type(variant, hotspot):
             return "2-indel"
     return "1-hotspot"
 
+
 def get_sample_value(variant, sample):
     if isinstance(variant, pysam.VariantRecord):
         return variant.samples[sample]
+
 
 def get_read_level(read_levels, rd):
     try:
@@ -68,16 +85,19 @@ def get_read_level(read_levels, rd):
         pass
     return "-", "zero"
 
+
 def is_indel(variant):
     if len(variant.alleles) != 2:
         raise Exception("Unhandled case: " + str(variant.alleles))
     return (len(variant.alleles[0]) == 1 and len(variant.alleles[1]) > 1) or \
            (len(variant.alleles[1]) == 1 and len(variant.alleles[0]) > 1)
 
+
 def is_multibp_sub(variant):
     if len(variant.alleles) != 2:
         raise Exception("Unhandled case: " + str(variant.alleles))
     return len(variant.alleles[0]) > 1 and len(variant.alleles[1]) > 1
+
 
 def clinical_flagged(variant):
     if isinstance(variant, pysam.VariantRecord):
@@ -87,6 +107,7 @@ def clinical_flagged(variant):
             return "Yes"
         else:
             return "No"
+
 
 def get_vaf(variant, sample):
     if isinstance(variant, pysam.VariantRecord):

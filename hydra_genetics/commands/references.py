95dac27fce6a568182eb3290e53066b7ade0e678
Patrik Smeds <patrik.smeds@gmail.com>
2023-05-26 15:00:57 +0200

    fix: don't look at pipeline files

--- hydra_genetics/commands/references.py
+++ hydra_genetics/commands/references.py
@@ -63,6 +63,12 @@ def validate(config_file, validation_file, path_to_ref_data):
         # docker container aren't files
         if ":" in possible_file:
             return False
+        # Skip files that are configured per analysis/site
+        if possible_file.endswith("samples.tsv") \
+           or possible_file.endswith("resources.tsv") \
+           or possible_file.endswith("units.tsv"):
+            logging.debug(f"Ignore: {possible_file}")
+            return False
         # If a extension can be found we consider it as a file
         if extension.suffix:
             return True

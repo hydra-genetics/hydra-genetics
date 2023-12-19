f72af9494d526867a554eded059e056bfef816c7
jonca79 <jonas.almlof@igp.uu.se>
2023-12-19 11:41:13 +0100

    fix: handle missing transcipt in annotation by using pick

--- hydra_genetics/utils/io/utils.py
+++ hydra_genetics/utils/io/utils.py
@@ -34,15 +34,18 @@ def get_annotation_data(data_extracter, mapper):
 
 def get_annotation_data_vep(field_dict, transcript_dict=None):
     def extractor(variant, info_name):
-        data = []
+        data = None
         if not transcript_dict:
             data = variant.info['CSQ'][0].split("|")[field_dict[info_name]]
         else:
             variant_key = f"{variant.chrom}_{variant.start}_{variant.stop}_{variant.ref}_{','.join(variant.alts)}"
+            print(variant_key)
             for transcript_data in variant.info['CSQ']:
                 if transcript_data.split("|")[field_dict['Feature']].startswith(transcript_dict[variant_key]):
                     data = transcript_data.split("|")[field_dict[info_name]]
-        if len(data) == 0:
+            if data is None:
+                data = variant.info['CSQ'][0].split("|")[field_dict[info_name]]
+        if data is None:
             return None
         return data
     return extractor

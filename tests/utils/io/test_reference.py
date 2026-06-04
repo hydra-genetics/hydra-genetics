d5a61f153ca9afc2968f0583cc37c3e0c4f5ec82
padraicc <padraic.corcoran@scilifelab.uu.se>
2026-06-04 14:36:57 +0200

    fix: support file:// URLs in fetch_url_content

--- tests/utils/io/test_reference.py
+++ tests/utils/io/test_reference.py
@@ -1,3 +1,5 @@
+import hashlib
+import os
 import shutil
 import tempfile
 import unittest
@@ -36,3 +38,29 @@ class TestReference(unittest.TestCase):
         self.assertEqual(len(failed), 1)
         self.assertEqual(len(links), 0)
         self.assertEqual(len(skipped), 0)
+
+    def test_fetch_reference_data_local_file(self):
+        content = b"local file content"
+        checksum = hashlib.md5(content).hexdigest()
+
+        # Create an actual local file to fetch via file:// URL
+        local_file = os.path.join(self.tmpdir, "test-source-file.txt")
+        with open(local_file, "wb") as f:
+            f.write(content)
+
+        validation_data = {
+            "local file": {
+                "path": "files/local-test-file.txt",
+                "type": "file",
+                "checksum": checksum,
+                "url": f"file://{local_file}",
+            },
+        }
+
+        fetched, links, failed, skipped = fetch_reference_data(validation_data, self.outdir)
+
+        self.assertEqual(len(fetched), 1)
+        self.assertEqual(len(failed), 0)
+        self.assertEqual(len(links), 0)
+        self.assertEqual(len(skipped), 0)
+        self.assertTrue(os.path.isfile(os.path.join(self.outdir, "files/local-test-file.txt")))

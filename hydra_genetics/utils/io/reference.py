4a30703131ee50159966b1c37bf7a845270987be
padraicc <padraic.corcoran@scilifelab.uu.se>
2026-06-04 14:44:13 +0200

    remove redundant coditional

--- hydra_genetics/utils/io/reference.py
+++ hydra_genetics/utils/io/reference.py
@@ -184,8 +184,7 @@ def fetch_url_content(url, content_holder, tmpdir):
             temp_file = os.path.join(tmpdir, f"file{counter}")
             list_of_temp_files.append(temp_file)
             if urlparse(part_url).scheme == "file":
-                if not copy_local_file(part_url, temp_file):
-                    return False
+                copy_local_file(part_url, temp_file)
             elif not download_with_retry(part_url, temp_file):
                 return False
             if not checksum_validate_file(temp_file, part_checksum):
